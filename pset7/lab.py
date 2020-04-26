f5619b3978f6325de2a75597d367d8e3507314a6
Sandra Tang <sandratang01@gmail.com>
2020-04-26 16:44:51 -0700

    Update lab.py

--- pset7/lab.py
+++ pset7/lab.py
@@ -4,265 +4,229 @@
 import doctest
 # NO ADDITIONAL IMPORTS!
 
-
 class EvaluationError(Exception):
-    """
-    Exception to be raised if there is an error during evaluation other than a
-    NameError.
-    """
-    pass
+	"""
+	Exception to be raised if there is an error during evaluation other than a
+	NameError.
+	"""
+	pass
 
 def tokenize(source):
-    """
-    Splits an input string into meaningful tokens (left parens, right parens,
-    other whitespace-separated values).  Returns a list of strings.
+	"""
+	Splits an input string into meaningful tokens (left parens, right parens,
+	other whitespace-separated values).  Returns a list of strings.
 
-    Arguments:
-        source (str): a string containing the source code of a carlae
-                      expression
-    """
-    lines = source.splitlines()
-    new = pre = ''
-    chars = ('(', ')', ' ')
-    for line in lines:
-        new += ' '
-        for c in line:
-            if c == ';':
-                break
-            if c in chars:
-                new += pre
-                pre = ''
-                new += ' ' + c + ' '
-            else:
-                pre += c
-        new += pre
-        pre = ''
-    new += pre
-    return new.split()
+	Arguments:
+		source (str): a string containing the source code of a carlae
+					  expression
+	"""
+	lines = source.splitlines()
+	new = pre = ''
+	chars = ('(', ')', ' ')
+	for line in lines:
+		new += ' '
+		for c in line:
+			if c == ';':
+				break
+			if c in chars:
+				new += pre
+				pre = ''
+				new += ' ' + c + ' '
+			else:
+				pre += c
+		new += pre
+		pre = ''
+	new += pre
+	return new.split()
 
 def parse(tokens):
-    """
-    Parses a list of tokens, constructing a representation where:
-        * symbols are represented as Python strings
-        * numbers are represented as Python ints or floats
-        * S-expressions are represented as Python lists
+	"""
+	Parses a list of tokens, constructing a representation where:
+		* symbols are represented as Python strings
+		* numbers are represented as Python ints or floats
+		* S-expressions are represented as Python lists
 
-    Arguments:
-        tokens (list): a list of strings representing tokens
-    """
-    if tokens.count('(') != tokens.count(')'):
-        raise SyntaxError
-    def convert(thing):
-        try:
-            return int(thing)
-        except:
-            try:
-                return float(thing)
-            except:
-                return thing
-    def parse_expression(index, sub):
-        # if first item is not '(' then must be standalone int, float, or var
-        if sub[0] == ')':
-            raise SyntaxError
-        if sub[0] != '(':
-            # return only item and index after last (0+1)
-            if len(sub) == 1:
-                return (convert(sub[0]), len(sub))
-            else:
-                raise SyntaxError
-        else:
-            result = []
-            # start at 1, assume first item is '('
-            i = 1
-            # start past first '(', count it in
-            left = 1
-            right = 0
-            while i < len(sub):
-                # print('IR', i, result)
-                t = sub[i]
-                if t == '(':
-                    left += 1
-                    # print(i, sub[i:])
-                    s, end = parse_expression(i, sub[i:])
-                    # print("SE", s, end)
-                    result.append(s)
-                    i = end
-                    # print('>>>', i, sub, len(sub))
-                    if i == len(sub):
-                        end = i-1
-                        break
-                    else:
-                        t = sub[i]
-                elif t == ')':
-                    right += 1
-                    i += 1
-                else:
-                    result.append(convert(t))
-                    i += 1
-                if left == right:
-                    # print('LR', left, right, i, index, sub)
-                    end = i-1
-                    break
-                if right > left:
-                    raise SyntaxError
-            return (result, end+1+index)
-    return parse_expression(0, tokens)[0]
+	Arguments:
+		tokens (list): a list of strings representing tokens
+	"""
+	if tokens.count('(') != tokens.count(')'):
+		raise SyntaxError
+	def convert(thing):
+		try:
+			return int(thing)
+		except:
+			try:
+				return float(thing)
+			except:
+				return thing
+	def parse_expression(index):
+		# if first item is not '(' then must be standalone int, float, or var
+		if tokens[index] == ')':
+			raise SyntaxError
+		if tokens[index] != '(':
+			return (convert(tokens[index]), index+1)
+		else:
+			result = []
+			# start past first '('
+			index += 1
+			while tokens[index] != ')':
+				s, end = parse_expression(index)
+				result.append(s)
+				index = end
+				if tokens[index] == ')':
+					break
+				if index >= len(tokens):
+					raise SyntaxError
+			return (result, end+1)
+	exp, e = parse_expression(0)
+	if e == len(tokens):
+		return exp
+	else:
+		raise SyntaxError
 
 def mult(lis):
-    prod = 1
-    for i in lis:
-        prod *= i
-    return prod
+	prod = 1
+	for i in lis:
+		prod *= i
+	return prod
 
 def div(lis):
-    quo = lis[0]
-    for i in lis[1:]:
-        quo /= i
-    return quo
+	quo = lis[0]
+	for i in lis[1:]:
+		quo /= i
+	return quo
 
 carlae_builtins = {
-    '+': sum,
-    '-': lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
-    '*': mult, 
-    '/': div
+	'+': sum,
+	'-': lambda args: -args[0] if len(args) == 1 else (args[0] - sum(args[1:])),
+	'*': mult, 
+	'/': div
 }
 
 class Environments:
-    def __init__(self, parent = None, variables = None):
-        self.parent = parent
-        if variables == None:
-            self.variables = {}
-        else:
-            self.variables = variables
-    def __setitem__(self, key, value):
-        self.variables[key] = value
-    def __getitem__(self, key):
-        if key in self.variables.keys():
-            return self.variables[key]
-        elif self.parent != None:
-            # go to parent environment
-            return self.parent[key]
-        else: #parent == None
-            raise NameError
-    def __delitem__(self, key):
-        del self.variables[key]
-    def __contains__(self, key):
-        if self.parent != None:
-            return key in self.variables.keys() or key in self.parent
-        else:
-            return key in self.variables.keys()
-    def __iter__(self):
-        for key, value in self.variables.items():
-            yield key, value
+	def __init__(self, parent = None, variables = None):
+		self.parent = parent
+		if variables == None:
+			self.variables = {}
+		else:
+			self.variables = variables
+	def __setitem__(self, key, value):
+		self.variables[key] = value
+	def __getitem__(self, key):
+		if key in self.variables.keys():
+			return self.variables[key]
+		elif self.parent != None:
+			# go to parent environment
+			return self.parent[key]
+		else: #parent == None
+			raise NameError
+	def __delitem__(self, key):
+		del self.variables[key]
+	def __contains__(self, key):
+		if self.parent != None:
+			return key in self.variables.keys() or key in self.parent
+		else:
+			return key in self.variables.keys()
+	def __iter__(self):
+		for key, value in self.variables.items():
+			yield key, value
 
 Carlae = Environments(None)
 Carlae.variables = carlae_builtins
 
 class Functions:
-    def __init__(self, params, expr, parent = Carlae):
-        self.params = params
-        self.expr = expr
-        self.parent = parent
-    def __call__(self, pa):
-        e = Environments()
-        for i in range(len(pa)):
-            f[params[i]] = pa[i]
-        return evaluate(self.expr, e)
+	def __init__(self, params, expr, parent = Carlae):
+		self.params = params
+		self.expr = expr
+		self.parent = parent
+	def __call__(self, pa):
+		if len(pa) != len(self.params):
+			raise EvaluationError
+		e = Environments(self.parent)
+		for i in range(len(pa)):
+			e[self.params[i]] = pa[i]
+		return evaluate(self.expr, e)
 
 def evaluate(tree, environment = None):
-    """
-    Evaluate the given syntax tree according to the rules of the carlae
-    language.
+	"""
+	Evaluate the given syntax tree according to the rules of the carlae
+	language.
 
-    Arguments:
-        tree (type varies): a fully parsed expression, as the output from the
-                            parse function
-    """
-    if environment is None:
-        environment = Environments(Carlae)
-    print(tree, environment, environment.variables)
-    print(">>>", tree[0] in environment)
-    if tree[0] in environment:
-        print(environment[tree[0]])
-        print('>>>', isinstance(environment[tree[0]], type(carlae_builtins['+'])))
-    # not list
-    if not isinstance(tree, list):
-        if isinstance(tree, str):
-            # print("ENVIRO:", environment[tree], environment.variables)
-            if tree in environment:
-                return environment[tree]
-            else:
-                raise NameError
-                # return tree
-        else:
-            return tree
-    # list (else)
-    if tree[0] in carlae_builtins.keys():
-        # contains list (needs recursive calls)
-        # if not all(not isinstance(item, list) for item in tree):
-        for index, item in enumerate(tree[1:]):
-            if isinstance(item, list):
-                tree[index+1] = evaluate(tree[index+1], environment)
-            elif isinstance(item, str):
-                tree[index+1] = environment[item]
-        # if all numbers
-        result = carlae_builtins[tree[0]](tree[1:])
-        if isinstance(result, list):
-            result = evaluate(result)
-    # is a defined function
-    elif tree[0] in environment and (isinstance(environment[tree[0]], type(tokenize)) or environment[tree[0]] in carlae_builtins):
-        for index, item in enumerate(tree[1:]):
-            if isinstance(item, list):
-                tree[index+1] = evaluate(tree[index+1], environment)
-            elif isinstance(item, str):
-                tree[index+1] = environment[item]
-        print("HEY")
-        result = environment[tree[0]](tree[1:])
-        print(result)
-    elif tree[0] == 'define':
-        if '(' not in tree[1] and ')' not in tree[1] and ' ' not in tree[1]:
-            if isinstance(tree[2], list) or isinstance(tree[2], str):
-                environment[tree[1]] = evaluate(tree[2], environment)
-            else:
-                environment[tree[1]] = tree[2]
-            return environment[tree[1]]
-    elif tree[0] == 'lambda':
-        if len(tree) == 4:
-            environment[tree[1]] = tree[3]
-            return evaluate(tree[2], environment)
-        else:
-            f = Functions(tree[1], tree[2])
-            return f(tree[1])
-    else:
-        print("HERE")
-        raise EvaluationError
-    return result
+	Arguments:
+		tree (type varies): a fully parsed expression, as the output from the
+							parse function
+	"""
+	print(tree, environment, environment.variables)
+	# default environment
+	if environment is None:
+		environment = Environments(Carlae)
+
+	# not list
+	if not isinstance(tree, list):
+		# handles variables
+		if isinstance(tree, str):
+			if tree in environment:
+				return environment[tree]
+			if tree in carlae_builtins:
+				return carlae_builtins[tree]
+			else:
+				raise NameError
+		# handles returning single numbers
+		else:
+			return tree
+	# list (else)
+	# is a defined function
+	if tree[0] == 'define':
+		if '(' not in tree[1] and ')' not in tree[1] and ' ' not in tree[1]:
+			if isinstance(tree[2], list) or isinstance(tree[2], str):
+				environment[tree[1]] = evaluate(tree[2], environment)
+			else:
+				environment[tree[1]] = tree[2]
+			return environment[tree[1]]
+	elif tree[0] == 'lambda':
+		f = Functions(tree[1], tree[2], environment)
+		return f
+	else:
+		try:
+			for index, item in enumerate(tree[1:]):
+				if isinstance(item, list):
+					tree[index+1] = evaluate(tree[index+1], environment)
+				elif isinstance(item, str):
+					tree[index+1] = environment[item]
+				else:
+					tree[index+1] = evaluate(tree[index+1], environment)
+			result = evaluate(tree[0], environment)(tree[1:])
+		except NameError:
+			raise NameError
+		except:
+			raise EvaluationError
+	return result
 
 def result_and_env(tree, environment = None):
-    if environment is None:
-        environment = Environments(Carlae)
-    return (evaluate(tree, environment), environment)
+	if environment is None:
+		environment = Environments(Carlae)
+	return (evaluate(tree, environment), environment)
 
 if __name__ == '__main__':
-    # code in this block will only be executed if lab.py is the main file being
-    # run (not when this module is imported)
-    pass
-    # uncommenting the following line will run doctests from above
-    # doctest.testmod()
-    # e = Environments(Carlae)
-    # inp = ''
-    # while (inp != 'QUIT'):
-    #     inp = input("Input: ")
-    #     print(">>>", inp)
-    #     inp_new = parse(tokenize(inp))
-    #     print("Output:", evaluate(inp_new, e))
-    # E = Environments()
-    # trees = ["(define dosomething +)", "(dosomething 2 3 4)"]
-    # for t in trees:
-    #     print(t)
-    #     t = tokenize(t)
-    #     print(t)
-    #     t = parse(t)
-    #     print(t)
-    #     print(evaluate(t, E))
-    #     print()
+	# code in this block will only be executed if lab.py is the main file being
+	# run (not when this module is imported)
+	pass
+	# uncommenting the following line will run doctests from above
+	# doctest.testmod()
+	# e = Environments(Carlae)
+	# inp = ''
+	# while (inp != 'QUIT'):
+	#     inp = input("Input: ")
+	#     print(">>>", inp)
+	#     inp_new = parse(tokenize(inp))
+	#     print("Output:", evaluate(inp_new, e))
+	E = Environments()
+	trees = ["(define addN (lambda (n) (lambda (i) (+ i n))))", "((addN 9) ((addN 9) 3))"]
+	for t in trees:
+		# print("T", t)
+		t = tokenize(t)
+		# print("TOKEN", t)
+		t = parse(t)
+		# print("PARSE", t)
+		print("EV", evaluate(t, E))
+		print()
